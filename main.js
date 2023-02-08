572393ffa6c18c2132621f7c45fea6d1b0225afc
altergodzin <123497643+altergodzin@users.noreply.github.com>
2023-02-07 22:05:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function newImage(){
+	fabric.Image.fromURL('BirthdayImage.jpg',function(Img){
+    
+    });
+	canvas.add(blockImageObject);
+};
+
+function playSound(){
+	x.play
+}
